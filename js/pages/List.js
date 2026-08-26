9d2eb2a4b009b109ce44c0262eb42e16c024da20
s4954941-del <s4954941@gmail.com>
2026-08-26 20:10:42 +0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -40,8 +40,7 @@ export default {
                 <div class="level" v-if="level">
                     <h1>{{ level.name }}</h1>
                     <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
-<iframe v-if="level.verification && (level.verification.includes('youtube') || level.verification.includes('youtu.be'))" :src="`https://www.youtube.com/embed/${embed(level.verification)}`" frameborder="0" allowfullscreen style="width: 100%; height: 100%;"></iframe>
-<img v-else-if="level.verification" :src="level.verification" :alt="level.name" style="width: 100%; height: 100%; object-fit: cover; border-radius: 8px;">
+ <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe> <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
                     <ul class="stats">
                         <li>
                             <div class="type-title-sm">Points when completed</div>
